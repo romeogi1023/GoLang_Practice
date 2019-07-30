8027c498a0e5c0b8e9f79f174e5bf2035a3d7586
DavidOffice <david@clacomusic.com>
2019-07-30 23:45:53 +0800

    找出長度大於10 的字串

--- test01-go-linq/main.go
+++ test01-go-linq/main.go
@@ -8,10 +8,29 @@ import (
 
 func main() {
 
+	testOrderBy()
+
+	testSingleWith()
+
+}
+
+// testSingleWith 找出長度大於10 的字串
+func testSingleWith() {
+	fruits := []string{"apple", "banana", "mango", "orange", "passionfruit", "grape"}
+
+	fruit := From(fruits).
+		SingleWith(
+			func(f interface{}) bool { return len(f.(string)) > 10 },
+		)
+
+	fmt.Println(fruit)
+}
+
+// testOrderBy 由小到大排序
+func testOrderBy() {
 	input := []int{10, 20, 40, 15, 18, 11, 25, 33}
 	var output []int
 
-	// 透過 go-linq 排序
 	From(input).OrderBy(func(i interface{}) interface{} { return i }).ToSlice(&output)
 
 	fmt.Println(output)
